d616d2a2ea15ec43e6435c3884003c569c0cbf72
Anthony LUCAS <a19lucas@srv-df-893.priv.enst-bretagne.fr>
2022-03-11 10:52:50 +0100

    fixed gitignore

--- build/lib/cli/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /homes/a19lucas/Documents/parping_galax/lib/cli
-# Build directory: /homes/a19lucas/Documents/parping_galax/build/lib/cli
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
